8bcec466628615e68d124e30bd667322aa349286
Jordan <jordan.conwaymclaughlin24@mail.dcu.ie>
2018-12-12 14:48:36 +0000

    sorted file reading

--- gallery/gen_gallery.py
+++ gallery/gen_gallery.py
@@ -1,4 +1,19 @@
 import os
 from os import listdir
+import sys
 
-print (listdir('img'))
+args = sys.argv[1:]
+article = args[0]
+columns = args[1]
+
+def read_file(s):
+    out = ''
+    with open(s) as f:
+        out = f.read()
+    return out
+
+gallery = read_file('gallery.html')
+column = read_file('column.html')
+img = read_file('img.html')
+
+print (listdir('../'+article+'/img'))
